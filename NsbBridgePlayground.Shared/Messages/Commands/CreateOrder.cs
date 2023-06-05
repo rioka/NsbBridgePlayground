de162a2fed38be1da128e9f09183afb7524286c9
rioka <rioka@libero.it>
2023-06-05 10:54:03 +0100

    Some shared code

---
+++ NsbBridgePlayground.Shared/Messages/Commands/CreateOrder.cs
@@ -0,0 +1,6 @@
+﻿namespace NsbBridgePlayground.Shared.Messages.Commands;
+
+public class CreateOrder
+{
+  public Guid Id { get; set; }
+}
---
+++ NsbBridgePlayground.Shared/Messages/Events/OrderCreated.cs
@@ -0,0 +1,6 @@
+namespace NsbBridgePlayground.Shared.Messages.Events;
+
+public class OrderCreated
+{
+  public Guid Id { get; set; }
+}
