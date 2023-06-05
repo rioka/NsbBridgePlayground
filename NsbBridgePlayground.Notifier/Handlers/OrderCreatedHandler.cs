644a995a6a45244a5ef2dd024181f3e6413e1606
rioka <rioka@libero.it>
2023-06-05 13:59:00 +0100

    New endpoint ("Notifier")

---
+++ NsbBridgePlayground.Notifier/Handlers/OrderCreatedHandler.cs
@@ -0,0 +1,22 @@
+using Microsoft.Extensions.Logging;
+using NsbBridgePlayground.Shared.Messages.Events;
+using NServiceBus;
+
+namespace NsbBridgePlayground.Notifier.Handlers;
+
+internal class OrderCreatedHandler : IHandleMessages<OrderCreated>
+{
+  private readonly ILogger<OrderCreatedHandler> _logger;
+
+  public OrderCreatedHandler(ILogger<OrderCreatedHandler> logger)
+  {
+    _logger = logger;
+  }
+  
+  /// <inheritdoc />
+  public Task Handle(OrderCreated message, IMessageHandlerContext context)
+  {
+    _logger.LogInformation("Processing {MessageType} for {OrderId}", nameof(OrderCreated), message.Id);
+    return Task.CompletedTask;
+  }
+}
---
+++ NsbBridgePlayground.Notifier/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NsbBridgePlayground.Shared;
+using NsbBridgePlayground.Shared.Infrastructure;
+using NServiceBus;
+
+namespace NsbBridgePlayground.Notifier;
+
+internal class Program
+{
+  public static async Task Main(string[] args)
+  {
+    var host = CreateHostBuilder(args)
+      .Build();
+
+    var config = host.Services.GetRequiredService<IConfiguration>();
+    await DbHelpers.EnsureDatabaseExists(config.GetConnectionString("Data"));
+
+    await host.RunAsync();
+  }
+
+  private static IHostBuilder CreateHostBuilder(string[] args)
+  {
+    var hb = Host
+      .CreateDefaultBuilder(args)
+      .UseConsoleLifetime()
+      .UseNServiceBus(ctx => {
+
+        var endpointConfig = Bootstrapper.Configure(Endpoints.Notifier, ctx.Configuration.GetConnectionString("Data"));
+        return endpointConfig;
+      });
+
+    return hb;
+  }
+}
