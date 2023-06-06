21fbd93dc86c92a886cf58cda9f3c6dcc712ab22
rioka <rioka@libero.it>
2023-06-06 16:09:21 +0100

    New project for the bridge

---
+++ NsbBridgePlayground.Bridge/Program.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using NsbBridgePlayground.Shared;
+
+namespace NsbBridgePlayground.Bridge;
+
+internal class Program
+{
+  public static async Task Main(string[] args)
+  {
+    var host = CreateHostBuilder(args)
+      .Build();
+
+    await host.RunAsync();
+  }
+
+  private static IHostBuilder CreateHostBuilder(string[] args)
+  {
+    var hb = Host
+      .CreateDefaultBuilder(args)
+      .UseConsoleLifetime()
+      .UseNServiceBusBridge((ctx, config) => {
+
+        AddConfiguration(config, Endpoints.Sender, ctx.Configuration.GetConnectionString("Sender"));
+        AddConfiguration(config, Endpoints.OrderProcessor, ctx.Configuration.GetConnectionString("OrderProcessor"));
+        AddConfiguration(config, Endpoints.Notifier, ctx.Configuration.GetConnectionString("Notifier"));
+        
+        config.RunInReceiveOnlyTransactionMode();
+      });
+
+    return hb;
+  }
+
+  private static void AddConfiguration(BridgeConfiguration bridgeConfig,
+    string endpoint,
+    string? connectionString,
+    string? nsbSchema = "nsb")
+  {
+    if (string.IsNullOrWhiteSpace(connectionString))
+    {
+      // log warning
+      return;
+    }
+
+    var sqlTransport = new SqlServerTransport(connectionString);
+    if (!string.IsNullOrWhiteSpace(nsbSchema))
+    {
+      sqlTransport.DefaultSchema = nsbSchema;
+    }
+    
+    var bridgeTransport = new BridgeTransport(sqlTransport) {
+      // all transports are SqlServerTransport, so we give each a name to be able to identify the proper one in logs
+      Name = $"For{endpoint}",
+      AutoCreateQueues = true
+    };
+    bridgeTransport.HasEndpoint(endpoint);
+    
+    bridgeConfig.AddTransport(bridgeTransport);
+  }
+}
--- README.md
+++ README.md
@@ -28,9 +28,16 @@ Initially, the system does not depend on the bridge.
 
 ## Required Changes
 
-- Commands do not need any specific configuration: we can drop any router-dependent settings. 
+- 💥Upgrade to NServiceBus v8
+
+  > We should plan accordingly.
+
+- A new service, analogous to the router, using `NServiceBus.Transport.Bridge`
+
+- Commands do not need any specific configuration: we can drop any router-dependent settings.
+
+  > This means we have to use plain NServiceBus API, i.e. `RoutingSettings.RouteToEndpoint` for commands.  
 
 ## References
 
 - [Migrating from NServiceBus.Router to NServiceBus.Transport.Bridge](https://docs.particular.net/nservicebus/bridge/migrating-from-router)
-
