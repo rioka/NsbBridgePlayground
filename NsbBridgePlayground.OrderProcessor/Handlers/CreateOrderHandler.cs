47a383c88812853fed533e67048fe4141e10363d
riccardo dozzo <rioka@libero.it>
2023-08-25 14:03:48 +0100

    Add business data (#7)

--- NsbBridgePlayground.OrderProcessor/Handlers/CreateOrderHandler.cs
+++ NsbBridgePlayground.OrderProcessor/Handlers/CreateOrderHandler.cs
@@ -1,13 +1,24 @@
+using Dapper;
 using Microsoft.Extensions.Logging;
 using NsbBridgePlayground.Common.Messages;
 using NsbBridgePlayground.Common.Messages.Commands;
 using NsbBridgePlayground.Common.Messages.Events;
 using NServiceBus;
+using NServiceBus.Persistence;
 
 namespace NsbBridgePlayground.OrderProcessor.Handlers;
 
 internal class CreateOrderHandler : IHandleMessages<CreateOrder>
 {
+  private static readonly string INSERT_ORDER = @"
+INSERT INTO [dbo].[Orders] (
+  [UId], [Notes], [CreatedAt]
+)
+OUTPUT inserted.id
+VALUES (
+  @uid, @notes, @createdAt
+)
+";
   private readonly ILogger<CreateOrderHandler> _logger;
 
   public CreateOrderHandler(ILogger<CreateOrderHandler> logger)
@@ -20,12 +31,26 @@ internal class CreateOrderHandler : IHandleMessages<CreateOrder>
   {
     _logger.LogInformation("Processing {MessageType} for {OrderId}", nameof(CreateOrder), message.Id);
 
+    var orderId = await RegisterOrder(message, context.SynchronizedStorageSession);
+    
     await context.Publish(new OrderCreated() {
       Id = message.Id
     });
     await context.Reply(new CreateOrderResponse() {
       Id = message.Id,
-      Notes = $"Created at {DateTime.UtcNow}"
+      Notes = $"Order created at {DateTime.UtcNow}"
     });
+  }
+
+  private async Task<int> RegisterOrder(CreateOrder order, SynchronizedStorageSession storageSession)
+  {
+    var cn = storageSession.SqlPersistenceSession().Connection;
+    var id = await cn.ExecuteScalarAsync<int>(INSERT_ORDER, new {
+      uid = order.Id,
+      notes = order.Notes,
+      createdAt = DateTime.UtcNow
+    });
+
+    return id;
   }
 }
