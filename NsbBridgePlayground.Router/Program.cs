b853e49d5612c8a4724f6cd749b8e87fb9823521
rioka <rioka@libero.it>
2023-09-21 10:37:38 +0100

    Add router (WIP)

---
+++ NsbBridgePlayground.Router/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+namespace NsbBridgePlayground.Router;
+
+internal class Program
+{
+  public static async Task Main(string[] args)
+  {
+    var host = CreateHostBuilder(args)
+      .Build();
+
+    await host.RunAsync();
+  }
+
+  private static IHostBuilder CreateHostBuilder(string[] args)
+  {
+    var hb = Host
+      .CreateDefaultBuilder(args)
+      .ConfigureAppConfiguration(builder => {
+
+        builder.AddEnvironmentVariables("NSBROUTER_");
+      })
+      .UseConsoleLifetime();
+
+    return hb;
+  }
+}
